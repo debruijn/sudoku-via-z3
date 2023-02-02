bf3135249d8b0b70746de3731844d4de4ad5c92e
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2023-02-02 20:45:43 +0100

    Working version of Sandwich Sudoku

--- sudoku_solver_alt.py
+++ sudoku_solver_alt.py
@@ -1,10 +1,34 @@
 import itertools
 import sys
 import z3
-from sudoku_solver import rows, cols, numbers, read_sudoku
+from sudoku_solver import rows, cols, numbers, sudoku_from_string
+from collections import defaultdict
 
 
-def solve_sudoku(known_values, variant=None):
+def read_sudoku(file):
+    """Reads a sudoku from a file-like object.
+
+    Args:
+        file: file object.
+
+    Returns: dictionary (int, int) -> int. See sudoku_from_string for details.
+    """
+    input = [line.strip() for line in file]
+    puzzle = sudoku_from_string("".join(input[0:9]))
+
+    extra = defaultdict(lambda: defaultdict(dict))
+    for row in input[9:]:
+        if row.startswith('sw_row'):
+            row_clean = row.lstrip('sw_row: ').replace(' ', '').split(',')
+            extra['sandwich']['row'] = {i: int(row_clean[i]) for i in range(len(row_clean)) if row_clean[i] != '.'}
+        if row.startswith('sw_col'):
+            row_clean = row.lstrip('sw_col: ').replace(' ', '').split(',')
+            extra['sandwich']['col'] = {i: int(row_clean[i]) for i in range(len(row_clean)) if row_clean[i] != '.'}
+
+    return puzzle, extra
+
+
+def solve_sudoku(known_values, variant=None, extra_input=None):
     """Solves a sudoku and prints its solution.
 
     Args:
@@ -21,7 +45,11 @@ def solve_sudoku(known_values, variant=None):
                     - diagonal
                     - queen
                     - windoku
+      extra_input:  Extra input needed for some variants, such as sandwich sudoku sums
     """
+    if extra_input is None:
+        extra_input = {}
+
     # Create a Z3 solver
     s = z3.Solver()
     # Create a matrix which is our sudoku.
@@ -34,7 +62,7 @@ def solve_sudoku(known_values, variant=None):
                 s.add(cells[r][c] == known_values[(r, c)])
 
     # This function adds all the constraints of a classic sudoku
-    add_constraints(s, cells, variant=variant)
+    add_constraints(s, cells, variant=variant, extra_input=extra_input)
 
     if s.check() == z3.sat:
         # Retrieve the model from the solver. In this model all the variables
@@ -55,12 +83,14 @@ def solve_sudoku(known_values, variant=None):
         print()
 
 
-def add_constraints(s, cells, variant=None):
+def add_constraints(s, cells, variant=None, extra_input=None):
+    if extra_input is None:
+        extra_input = {}
     if variant is None:
         variant = []
     classic_constraints(s, cells)
     if 'sandwich' in variant:
-        sandwich_constraints(s, cells)
+        sandwich_constraints(s, cells, extra_input)
 
 
 def z3_sum_between(vec, x1, x2):
@@ -69,26 +99,29 @@ def z3_sum_between(vec, x1, x2):
                  z3.Sum([z3.If(z3.And(i > x2, i < x1), vec[i], 0) for i in cols()]))
 
 
-def sandwich_constraints(s, cells):
-    sw = 5  # In row 6 (7th row)
-    cells_hor = [[None for _ in numbers()] for _ in rows()]  # Interpretation: y[r][i] is where i is in row r
+def sandwich_constraints(s, cells, extra_input):
+    cells_transpose = [z3.Array(f'tc_{r}', z3.IntSort(), z3.IntSort()) for r in rows()]
+    [[s.add(cells[r][c] == cells_transpose[c][r]) for r in rows()] for c in cols()]
+
     for r in rows():
-        if r == 6:
+        if r in extra_input['sandwich']['row']:
             x1 = z3.Int(f'sw1_r_{r}')
             x9 = z3.Int(f'sw9_r_{r}')
             s.add(x1 >= 0, x9 >= 0)
             s.add(x1 < 9, x9 < 9)
             s.add(cells[r][x1] == 1)
             s.add(cells[r][x9] == 9)
-            s.add(z3_sum_between(cells[r], x1, x9) == sw)
-        # for i in numbers():
-        #     # Z3 variables have a name
-        #     v = z3.Int('y_%d_%d' % (r, i))
-        #     # Keep a reference to the Z3 variable in our sudoku.
-        #     cells_hor[r][i] = v
-        #     s.add(z3.eq(i, cells[r][cells_hor[r][i]]))
-        #     #  https://stackoverflow.com/questions/49474310/how-to-use-z3-bitvec-or-int-as-an-array-index
-        #     # use z3.Select and replace cells with actual z3 Array
+            s.add(z3_sum_between(cells[r], x1, x9) == extra_input['sandwich']['row'][r])
+    for c in cols():
+        if c in extra_input['sandwich']['col']:
+            x1 = z3.Int(f'sw1_c_{c}')
+            x9 = z3.Int(f'sw9_c_{c}')
+            s.add(x1 >= 0, x9 >= 0)
+            s.add(x1 < 9, x9 < 9)
+            s.add(cells_transpose[c][x1] == 1)
+            s.add(cells_transpose[c][x9] == 9)
+            col = [cells[r][c] for r in rows()]
+            s.add(z3_sum_between(col, x1, x9) == extra_input['sandwich']['col'][c])
 
 
 def classic_constraints(s, cells):
@@ -130,8 +163,8 @@ def classic_constraints(s, cells):
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
         with open(sys.argv[1]) as f:
-            input_values = read_sudoku(f)
+            input_values, extra_input = read_sudoku(f)
     else:
-        input_values = read_sudoku(sys.stdin)
+        input_values, extra_input = read_sudoku(sys.stdin)
     variant_input = sys.argv[2:] if len(sys.argv) >= 3 else ['classic']
-    solve_sudoku(input_values, variant_input)
+    solve_sudoku(input_values, variant_input, extra_input)
